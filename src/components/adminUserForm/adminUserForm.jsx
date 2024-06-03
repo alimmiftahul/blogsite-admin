c2fe721c86b48065b0def8002a3eedf0ee508ebe
alimmiftahul <alimmiftaul08@gmail.com>
2024-06-03 20:06:52 +0700

    Add user form  for admin panel

--- src/components/adminUserForm/adminUserForm.jsx
+++ src/components/adminUserForm/adminUserForm.jsx
@@ -1,8 +1,26 @@
+'use client';
+
 import React from 'react';
 import styles from './adminUserForm.module.css';
+import { useFormState } from 'react-dom';
+import { addUser } from '@/lib/action';
 
 const AdminUserForm = () => {
-    return <div>adminUserForm</div>;
+    const [state, formAction] = useFormState(addUser, undefined);
+    return (
+        <form action={formAction}>
+            <h1>add user</h1>
+            <input type="text" placeholder="username" name="username" />
+            <input type="text" placeholder="email" name="email" />
+            <input type="password" placeholder="password" name="password" />
+            <input
+                type="password"
+                placeholder="confirm Password"
+                name="confirmPassword"
+            />
+            <button>Submit</button>
+        </form>
+    );
 };
 
 export default AdminUserForm;
